9d1b515d572e97731e5ab488205a2df99e182147
Adam (Padre) <padreadamo@gmail.com>
2023-07-02 20:44:16 -0400

    Update Player_Combat.cs
    natural resistance and regen bonus from end/str updated and re-scaled

--- Source/ACE.Server/WorldObjects/Player_Combat.cs
+++ Source/ACE.Server/WorldObjects/Player_Combat.cs
@@ -991,11 +991,11 @@ namespace ACE.Server.WorldObjects
         {
             var strAndEnd = Strength.Base + Endurance.Base;
 
-            if (strAndEnd > 440)        return "Indomitable";
-            else if (strAndEnd > 380)   return "Resilient";
-            else if (strAndEnd > 320)   return "Hardy";
-            else if (strAndEnd > 260)   return "Mediocre";
-            else if (strAndEnd > 200)   return "Poor";
+            if (strAndEnd > 400)        return "Indomitable";
+            else if (strAndEnd > 360)   return "Resilient";
+            else if (strAndEnd > 300)   return "Hardy";
+            else if (strAndEnd > 200)   return "Mediocre";
+            else if (strAndEnd > 100)   return "Poor";
             else
                 return "None";
         }
@@ -1004,11 +1004,11 @@ namespace ACE.Server.WorldObjects
         {
             var strAndEnd = Strength.Base + 2 * Endurance.Base;
 
-            if (strAndEnd > 690)        return "Indomitable";
-            else if (strAndEnd > 580)   return "Resilient";
-            else if (strAndEnd > 470)   return "Hardy";
-            else if (strAndEnd > 346)   return "Mediocre";
-            else if (strAndEnd > 200)   return "Poor";
+            if (strAndEnd > 440)        return "Indomitable";
+            else if (strAndEnd > 560)   return "Resilient";
+            else if (strAndEnd > 300)   return "Hardy";
+            else if (strAndEnd > 200)   return "Mediocre";
+            else if (strAndEnd > 100)   return "Poor";
             else
                 return "None";
         }
