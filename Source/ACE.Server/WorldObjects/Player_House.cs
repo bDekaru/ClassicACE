e75aa3055b78872e1bfa3a551b62e0419cb79110
Dekaru <dekaru@protonmail.com>
2025-03-12 15:44:13 -0300

    CustomDM: Fix for infinite teleport loop when /house recalling to a bunk which is very close to another bunk.

--- Source/ACE.Server/WorldObjects/Player_House.cs
+++ Source/ACE.Server/WorldObjects/Player_House.cs
@@ -1459,7 +1459,7 @@ namespace ACE.Server.WorldObjects
             {
                 var rootHouse = house.RootHouse;
 
-                if (!rootHouse.OnProperty(this))
+                if (rootHouse.IsCustomHouse || !rootHouse.OnProperty(this))
                     continue;
 
                 if (rootHouse.HouseOwner != null && !rootHouse.HasPermission(this, false))
